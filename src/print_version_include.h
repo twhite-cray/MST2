bccbdccd19fb152804229e2355ee01e292a2f84f
Trey White <twhite@cray.com>
2019-11-21 16:24:50 -0600

    Changed Fortran unit 101 to 103. Fixed module path in poplar_v100.

--- src/print_version_include.h
+++ src/print_version_include.h
@@ -4,4 +4,4 @@
 ! 
    write(myunit,'("# ",t5,a,t30,": ",a)') "username",trim("twhite")
    write(myunit,'("# ",t5,a,t30,": ",a)') "system name",trim("poplar.cm.cluster")
-   write(myunit,'("# ",t5,a,t30,": ",a)') "code compiled",trim("Tuesday, November 19 2019 19:24:50")
+   write(myunit,'("# ",t5,a,t30,": ",a)') "code compiled",trim("Thursday, November 21 2019 15:56:25")
